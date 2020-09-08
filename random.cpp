99638dcc5fae5f9fb2be27453c33333373a449d2
samuelelanzi <samuelelanzi00@icloud.com>
2020-09-08 17:45:23 +0200

    Random

--- random.cpp
+++ random.cpp
@@ -1,14 +1,38 @@
 #include <iostream>
 #include <random>
+#include <stdexcept>
+#include <vector>
 
-int Rand() {
-	std::random_device rd;
-	std::mt19937 gen(rd());
-	std::uniform_int_distribution<> distrib(1, 6);
-	return distrib(gen);
+std::vector<int> VectorRi (int const& n, int const& A, int const& B) {
+  if (n <= 0) {
+    throw std::invalid_argument("Il vettore è vuoto");
+  }
+
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<> distrib(A, B);
+  std::vector<int> v {};
+  for (int i = 0; i != n; ++i) {
+    v.push_back(distrib(gen));
+  }
+  return v;
 }
 
+void print(std::vector<int> const& v) {
+  for(auto const& i : v) {
+    std::cout << i << ' ';
+  }
+  std::cout << '\n';
+}
+
+
 int main() {
-	auto a = Rand();
-	std::cout << a << '\n';
+  try {
+    int N;
+    std::cin >> N;
+    std::vector<int> v1 = VectorRi(N, 0, 50);
+    print(v1);
+  } catch (std::invalid_argument& e) {
+    std::cerr << e.what() << '\n';
+  }
 }
