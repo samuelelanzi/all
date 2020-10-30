ea8fdf5a4162bf11c07e5ffa022dc71ebccabd07
Samuele Lanzi <65108938+samuelelanzi@users.noreply.github.com>
2020-10-30 22:00:38 +0100

    Add files via upload

---
+++ code/motion/exp.cpp
@@ -0,0 +1,21 @@
+void exp() {
+    TGraph* graph = new TGraph("data.txt","%lg %lg"); 
+    
+    graph->SetTitle("Esperimento ; t [s] ; x(t) [m]");
+    graph->SetMarkerStyle(kOpenCircle);
+    graph->SetMarkerColor(kBlue);
+    graph->SetLineColor(kBlue);
+
+    TCanvas *mycanvas = new TCanvas();
+
+    TF1* f = new TF1("Quadratic Law", "0.017 + x * x * [0]", 0.0, 0.9);
+
+    f->SetLineColor(kRed); 
+    f->SetLineStyle(2);
+ 
+    graph->Fit(f);
+    gStyle->SetOptFit(111);
+    graph->Draw("APE");
+    
+    mycanvas->Print("motion.pdf");
+}
